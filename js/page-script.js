fcebcc48f2ed0660b96e2f42efcae127be3f9fa7
owolana ayomide <ayomideowolana1@gmail.com>
2022-02-16 14:39:11 +0100

    updated faq layout and js

--- js/page-script.js
+++ js/page-script.js
@@ -1,5 +1,12 @@
 $(document).ready(() => {
     $("#nav-toggle").click(()=>{
         $("#nav-mobile-links").toggle(300)
-    })
+    });
+    $(".faq > p").click((e)=>{
+        let elem =  e.target.id
+        $(`.${elem}`).toggle(300)
+        // console.log(elem)
+        $(`#faqs .faq-hide:not(.${elem})`).hide(300)
+
+    });
 });
