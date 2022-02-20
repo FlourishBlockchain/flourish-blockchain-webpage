9580fba988e25757c7ab68476a3f66b831851ccc
owolana ayomide <ayomideowolana1@gmail.com>
2022-02-20 20:01:31 +0100

    added FAQ sub-section, added icons for footer and edited team section layout

--- js/page-script.js
+++ js/page-script.js
@@ -1,14 +1,23 @@
 $(document).ready(() => {
-    $("#nav-toggle").click(()=>{
-        $("#nav-mobile-links").toggle(300)
-    });
-    $(".faq > p").click((e)=>{
-        let elem =  e.target.id
-        $(`.${elem}`).toggle(300)
-        // console.log(elem)
-        $(`#faqs .faq-hide:not(.${elem})`).hide()
-    });
-    $("#nav-mobile-links a").click(()=>{
-        $("#nav-mobile-links").hide(100)
-    })
+  $("#nav-toggle").click(() => {
+    $("#nav-mobile-links").toggle(300);
+  });
+
+  $(".faq > p").click(e => {
+    let elem = e.target.id;
+    $(`.${elem}`).toggle(300);
+    // console.log(elem)
+    $(`#faqs .faq-hide:not(.${elem})`).hide();
+  });
+
+  $(".faq-sub > p").click(e => {
+    let elem = e.target.id;
+    $(`.${elem}`).toggle(300);
+    // console.log(elem)
+    $(`#faq-subs .faq-hide:not(.${elem})`).hide();
+  });
+
+  $("#nav-mobile-links a").click(() => {
+    $("#nav-mobile-links").hide(100);
+  });
 });
